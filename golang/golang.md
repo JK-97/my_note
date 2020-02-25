fb79f026a6bdfca55b474975af107a495027c3e6
JK-97 <benjamin-dream@outlook.com>
2020-02-25 22:41:14 +0800

    func reload

--- golang/golang.md
+++ golang/golang.md
@@ -5,3 +5,5 @@
 ![1_range_map.png ](https://jk-97.github.io/my_note/golang/sources/images/1_range_map.png)
 **result:**
 ![1_range_map_res.png ](https://jk-97.github.io/my_note/golang/sources/images/1_range_map_res.png)
+**solve**
+需要在range体内声明一个变量来存储value，再将指针赋予map
---
+++ golang/sources/code/func_reload/func_reload.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type People struct{}
+
+func (p *People) ShowA() {
+	fmt.Println("showA")
+	p.ShowB()
+}
+func (p *People) ShowB() {
+	fmt.Println("showB")
+}
+
+type Teacher struct {
+	People
+}
+
+func (t *Teacher) ShowB() {
+	fmt.Println("teacher showB")
+}
+
+func main() {
+	t := Teacher{}
+	t.ShowA()
+	x := "Aaaaaa"
+	for index, key := range x {
+		fmt.Println(index, key)
+	}
+}
